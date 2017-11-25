8f2000db4aedc5f245158ca44688a2ff835bb1b1
FunGumz <squall11135@hotmail.com>
2017-11-25 11:52:32 +0700

    เพิ่มโฟลเดอร์ Go_Programming
    เพิ่มโฟลเดอร์งาน

---
+++ Go_Programming/Golang_Season1/Project_11_Variadic-Function.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+  summation(9, 10)
+}
+
+func summation(args ...int)  {
+  var total int
+  for _ ,n := range args {
+    total += n
+  }
+  fmt.Println(total)
+}
---
+++ Go_Programming/Golang_Season1/Project_19_Go-Routine.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+  go f(0)
+  var input string
+  fmt.Scanln(&input)
+}
+
+func f(n int)  {
+  for i := 1; i <= 10; i++ {
+    fmt.Println(n, " : ", i)
+  }
+}
